519dc9782cc2e5f224fdfe585feb0682c4558d82
sultanalnhari <sultanalnhari@localhost>
2020-11-26 14:24:01 -0700

    reading file

--- src/EReaderMain.java
+++ src/EReaderMain.java
@@ -1,4 +1,7 @@
 public class EReaderMain {
+	public void main(String[] arg) {
+		EReaderModel model = new EReaderModel();
 
+	}
 	
 }
--- src/EReaderModel.java
+++ src/EReaderModel.java
@@ -1,4 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class EReaderModel {
-	
+	public EReaderModel() {
+		try {
+			File myObj = new File("book1.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Error");
+			e.printStackTrace();
+		}
+
+	}
 	
 }
