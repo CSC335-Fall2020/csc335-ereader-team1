c82d759d4db785eeef60d0f14741de0cf1f53f16
alihamzanoor <alihamzanoor@localhost>
2020-11-26 15:55:15 -0700

    Initial Model Commit

--- src/EReaderMain.java
+++ src/EReaderMain.java
@@ -1,3 +1,9 @@
-public class EReaderMain {
+import java.util.Arrays;
 
+public class EReaderMain {
+	public static void main(String[] args) {
+		EReaderModel model = new EReaderModel();
+		String[] array = model.getLines("book1.txt").get(1);
+		System.out.print(Arrays.deepToString(array));
+	}
 }
