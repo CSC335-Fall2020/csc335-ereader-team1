86b1cdcd0ff01480fdf654e616de7e45e6fc32cb
jackguerin <jackguerin@localhost>
2020-11-26 22:35:16 -0700

    Added launch in main to launch the view.

--- src/EReaderMain.java
+++ src/EReaderMain.java
@@ -1,3 +1,8 @@
-public class EReaderMain {
+import javafx.application.Application;
 
+public class EReaderMain {
+	
+	public static void main(String[] args) {
+		Application.launch(EReaderView.class, args);
+	}
 }
