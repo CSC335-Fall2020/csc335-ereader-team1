a70ec0e42690b20836167b0a4c606e049fa4117a
sultanalnhari <sultanalnhari@localhost>
2020-12-01 04:01:10 -0700

    trying to show text on screen

--- src/EReaderMain.java
+++ src/EReaderMain.java
@@ -2,8 +2,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javafx.application.Application;
+
 public class EReaderMain {
 	public static void main(String[] args) {
+		Application.launch(EReaderView.class, args);		
+		
 		EReaderModel model = new EReaderModel();
 		EReaderController controller = new EReaderController(model);
 		ArrayList<String[]> array = controller.getLines("book1.txt");
